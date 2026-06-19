87b4cd6aa9f6ae78d49f6dd6584ec2b2b7b0b94d
Daniel Azevedo <daniel.azevedo.maia@hotmail.com>
2026-06-19 15:01:17 -0300

    feat: add health check endpoint

---
+++ src/test/java/com/danielmaia/musiccatalog/common/health/HealthControllerTest.java
@@ -0,0 +1,28 @@
+package com.danielmaia.musiccatalog.common.health;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HealthController.class)
+class HealthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Should return API health status")
+    void shouldReturnApiHealthStatus() throws Exception {
+        mockMvc.perform(get("/api/v1/health"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status", is("UP")));
+    }
+
+}
