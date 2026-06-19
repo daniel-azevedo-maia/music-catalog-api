99d668ba5ee0bc4a1403d9c93cbca93aae21b665
Daniel Azevedo <daniel.azevedo.maia@hotmail.com>
2026-06-19 14:53:48 -0300

    feat: create initial Spring Boot project

---
+++ src/main/java/com/danielmaia/musiccatalog/MusicCatalogApiApplication.java
@@ -0,0 +1,13 @@
+package com.danielmaia.musiccatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicCatalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicCatalogApiApplication.class, args);
+	}
+
+}
