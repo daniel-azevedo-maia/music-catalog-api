bf702adeb0480de08d2fcfa5d4577cbe4ea02eba
Daniel Azevedo <daniel.azevedo.maia@hotmail.com>
2026-06-19 15:30:40 -0300

    chore: configure application profiles and docker compose

--- src/test/java/com/danielmaia/musiccatalog/common/health/HealthControllerTest.java
+++ src/test/java/com/danielmaia/musiccatalog/common/health/HealthControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
@@ -12,6 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(HealthController.class)
+@ActiveProfiles("test")
 class HealthControllerTest {
 
     @Autowired
